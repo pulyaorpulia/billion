b44ae57ca7857c021001114dead659656fbe1473
[] <pulyaa1998@mail.ru>
2025-03-13 10:07:30 +0500

    'ppp'

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
+    const burger = document.querySelector('.burger');
     navLinks.classList.toggle('active');
+    burger.classList.toggle('active');
 }
 
 function closeMenu() {
     const navLinks = document.querySelector('.nav-links');
+    const burger = document.querySelector('.burger');
     navLinks.classList.remove('active');
+    burger.classList.remove('active');
 }
 
 // Har bir linkka yopish funksiyasini qo'shish
